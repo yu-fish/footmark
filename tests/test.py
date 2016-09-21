83fae5216d0e8b524284bbb527059ad9886b0e2a
xiaozhu36 <guimin.hgm@alibaba-inc.com>
2016-09-21 10:24:21 +0800

    delete result and modify test.py

--- tests/test.py
+++ tests/test.py
@@ -3,9 +3,8 @@
 import argparse
 import os
 import sys
+
 from nose.core import run
-from nose_htmloutput import HtmlOutput
-import time
 
 
 def main():
@@ -16,14 +15,15 @@ def main():
     parser.add_argument('-t', '--service-tests', action="append", default=[],
                         help="Run tests for a given service.  This will "
                         "run any test tagged with the specified value, "
-                        "e.g -t ecs -t oss")
+                        "e.g -t test_instance_attribute -t test_manage_instances")
     known_args, remaining_args = parser.parse_known_args()
     attribute_args = []
-    filename = os.path.dirname(os.path.abspath(__file__)) + "/results/" + time.strftime('%Y%m%d%H%M%S', time.localtime(time.time())) + ".html"
+    for service_attribute in known_args.service_tests:
+        attribute_args.extend(['-a', '!notdefault,' + service_attribute])
     if not attribute_args:
         # If the user did not specify any filtering criteria, we at least
         # will filter out any test tagged 'notdefault'.
-        attribute_args = ['-v','--with-html', '--html-file='+filename]
+        attribute_args = ['-a', '!notdefault']
 
     # Set default tests used by e.g. tox. For Py2 this means all unit
     # tests, while for Py3 it's just whitelisted ones.
@@ -36,9 +36,12 @@ def main():
                 remaining_args[i] = 'tests/unit'
 
     all_args = [__file__] + attribute_args + remaining_args
-    print("nose command:", ' '.join(all_args))
-
-    run(argv=all_args, plugins=[HtmlOutput()])
+    if run(argv=all_args):
+        # run will return True is all the tests pass.  We want
+        # this to equal a 0 rc
+        return 0
+    else:
+        return 1
 
 if __name__ == "__main__":
     sys.exit(main())
